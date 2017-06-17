9bda05636b3aec7e9b181b31378ed83e92dd37e9
piganeau <gregoire.piganeau@telecom-paristech.fr>
2017-06-17 15:44:49 +0200

    Theseus all done, with nice animations. Use as template for monsters by removing the parent 2D Node that is useless

--- Game/scripts/theseus_kinematic_body.gd
+++ Game/scripts/theseus_kinematic_body.gd
@@ -4,6 +4,7 @@ extends KinematicBody2D
 # var a = 2
 # var b = "textvar"
 var original_pos
+const MOVEMENT_UNIT = 50
 
 func _ready():
 	original_pos = get_node(".").get_global_pos()
@@ -11,27 +12,38 @@ func _ready():
 	# Initialization here
 	
 func move_up():
-	move(Vector2(0,-60))
-	#if (is_colliding()):
-	#	revert_motion()
+	move(Vector2(0,-MOVEMENT_UNIT))
+	if (is_colliding()):
+		revert_motion()
+		get_node("Movement_anims").play("blocked_move_up")
+	else:
+		get_node("Movement_anims").play("movement_up_50px")
 
 
 func move_down():
-	move(Vector2(0,50))
-	#get_node("Movement_down").play("movement_down")
+	move(Vector2(0,MOVEMENT_UNIT))
 	if (is_colliding()):
 		revert_motion()
+		get_node("Movement_anims").play("blocked_move_down")
+	else:
+		get_node("Movement_anims").play("movement_down_50px")
 
 
 func move_left():
-	move(Vector2(-50,0))
+	move(Vector2(-MOVEMENT_UNIT,0))
 	if (is_colliding()):
 		revert_motion()
+		get_node("Movement_anims").play("blocked_move_left")
+	else:
+		get_node("Movement_anims").play("movement_left_50px")
 
 func move_right():
-	move(Vector2(50,0))
+	move(Vector2(MOVEMENT_UNIT,0))
 	if (is_colliding()):
 		revert_motion()
+		get_node("Movement_anims").play("blocked_move_right")
+	else:
+		get_node("Movement_anims").play("movement_right_50px")
 
 func _on_swipe_gesture_swiped( gesture ):
 	var dir = gesture.get_direction()
