9c8ba78d32bef91bba2dc0d70bc519cb8276f5c5
Jean-Raphaël Cornel <jean-raphael.cornel@telecom-paristech.fr>
2017-06-19 14:30:51 +0200

    reprise du système de portes. dézoom de la caméra

--- Game/scripts/test_map.gd
+++ Game/scripts/test_map.gd
@@ -2,15 +2,33 @@ extends Node
 
 var hero_position
 var direction
+var doors_locations
 
 const hero_speed = 50
 
 func _ready():
+	doors_locations = Vector2Array()
+	find_doors()
+	close_doors()
 	set_process(true)
 
 func _process(delta):
 	if (get_tree().get_nodes_in_group("enemies").size()==0):
-		var tab = get_node("TileMap").get_used_cells()
-		for t in range(tab.size()):
-			if (get_node("TileMap").get_cell(tab[t][0],tab[t][1])==2):
-				get_node("TileMap").set_cell(tab[t][0],tab[t][1],3)
+		open_doors()
+
+
+func open_doors():
+	for d in range(doors_locations.size()):
+		if (get_node("TileMap").get_cell(doors_locations[d][0],doors_locations[d][1])==2):
+			get_node("TileMap").set_cell(doors_locations[d][0],doors_locations[d][1],3)
+
+func close_doors():
+	for d in range(doors_locations.size()):
+		if (get_node("TileMap").get_cell(doors_locations[d][0],doors_locations[d][1])==3):
+			get_node("TileMap").set_cell(doors_locations[d][0],doors_locations[d][1],2)
+
+func find_doors():
+	var tab = get_node("TileMap").get_used_cells()
+	for t in range(tab.size()):
+		if (get_node("TileMap").get_cell(tab[t][0],tab[t][1])==3 or get_node("TileMap").get_cell(tab[t][0],tab[t][1])==2 ):
+			doors_locations.append(tab[t])
