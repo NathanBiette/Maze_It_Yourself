44efb42ce30f62a059f007d42420a4827676aff1
Jean-Raphaël Cornel <jean-raphael.cornel@telecom-paristech.fr>
2017-06-19 11:53:50 +0200

    a door opens when all enemies are killed

--- Game/scripts/test_map.gd
+++ Game/scripts/test_map.gd
@@ -6,4 +6,11 @@ var direction
 const hero_speed = 50
 
 func _ready():
-	pass
+	set_process(true)
+
+func _process(delta):
+	if (get_tree().get_nodes_in_group("enemies").size()==0):
+		var tab = get_node("TileMap").get_used_cells()
+		for t in range(tab.size()):
+			if (get_node("TileMap").get_cell(tab[t][0],tab[t][1])==2):
+				get_node("TileMap").set_cell(tab[t][0],tab[t][1],3)
