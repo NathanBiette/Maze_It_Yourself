2912fede49f9d57f2cfcdc86c98f831b787dcac6
Jean-Raphaël Cornel <jean-raphael.cornel@telecom-paristech.fr>
2017-06-26 17:28:13 +0200

    adds every rooms, connects doors randomly, and spawns skeletons

--- Game/demo.gd
+++ Game/demo.gd
@@ -27,10 +27,36 @@ func _ready():
 	level_instance = level_scene.instance()
 	add_child(level_instance)
 	for k in range(0,4):
-		print(k)
 		level_instance.add_room(k)
-	
-	
+	var  doors=level_instance.get_doors()
+	print(doors)
+	var size = doors.size()
+	while (doors.size()>0):
+		size=doors.size()
+		var end_early=true
+		for k in range(size-1):
+			if(doors[k][1][0] != doors[k+1][1][0]):
+				end_early=false
+		if(!end_early):
+			var k = 0
+			var i = 0
+			while(doors[i][1][0]==doors[k][1][0]):
+				i=randi()%size
+				k=randi()%size
+			level_instance.connect(doors[i][1],doors[k][1])
+			if(k>i):
+				doors.remove(k)
+				doors.remove(i)
+			else:
+				doors.remove(i)
+				doors.remove(k)
+		elif(end_early):
+			doors = []
+	var spawns = level_instance.get_spawns()
+	var k = 0
+	for k in range(spawns.size()):
+		level_instance.link(spawns[k][0],"skeleton")
+	level_instance.update_release()
 
 func game_over():
 	get_node("..").game_over()
