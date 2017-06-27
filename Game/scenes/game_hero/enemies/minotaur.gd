baa9a4ca30af4199f58530b282f3aaae15c3a280
piganeau <gregoire.piganeau@telecom-paristech.fr>
2017-06-27 17:42:01 +0200

    Rare bug where Minotaur stops moving if player moves when he attacks, tried fix

--- Game/scenes/game_hero/enemies/minotaur.gd
+++ Game/scenes/game_hero/enemies/minotaur.gd
@@ -2,11 +2,12 @@ extends KinematicBody2D
 
 var facing = "down"
 var damage = 2
-var health = 50
+var health
 var gold = 100
 var is_dead = false
 
 func _ready():
+	health = 50
 	get_node("theseus_detection_ray").set_enabled(true)
 	get_node("theseus_detection_ray").add_exception(self)
 	get_node("theseus_detection_ray").add_exception(get_node(".."))
@@ -29,7 +30,15 @@ func _process(delta):
 	if health <= 0:
 		is_dead = true
 		get_node("CollisionShape2D").queue_free()
-		get_node("AnimatedSprite/Movement_anims").stop_all()
+		get_node("AnimatedSprite").stop()
+		get_node("AnimatedSprite/Movement_anims").queue_free()
+		get_node("AnimatedSprite/Windup_anims").queue_free()
+		get_node("AnimatedSprite/Attack_anims").queue_free()
+		get_node("AnimatedSprite/Attack_anims_windup").queue_free()
+		get_node("AnimatedSprite/Charge_anims").queue_free()
+		get_node("AnimatedSprite/Circular_strike_anim").queue_free()
+		get_node("AnimatedSprite/Circular_strike_windup").queue_free()
+		get_node("AnimatedSprite/Idle_anims").queue_free()
 		get_node("AnimatedSprite/Death_anims").play("death_"+str(facing))
 		set_process(false)
 
@@ -43,7 +52,7 @@ func next_action():
 	var theseus_pos = get_node("../../../../theseus").get_global_pos() 
 	var guideline = get_node(".").get_global_pos() - theseus_pos
 	var angle = guideline.angle()
-	get_node("theseus_detection_ray").set_cast_to(-guideline.normalized() * 150)
+	get_node("theseus_detection_ray").set_cast_to(-guideline.normalized() * 200)
 	if get_node("theseus_detection_ray").is_colliding():
 		#Minautor attacks
 		if (angle < 0.785 and angle > -0.785) :
@@ -63,32 +72,48 @@ func next_action():
 			get_node("AnimatedSprite/Attack_anims_windup").play("attack_down_windup")
 			facing = "down"
 	elif (get_node("theseus_detection_ray").get_cast_to().x == 0 or get_node("theseus_detection_ray").get_cast_to().y == 0):
-		pass
+		get_node("AnimatedSprite/Idle_anims").play("idle_"+str(facing))
 	else:
 		if (angle < 0.785 and angle > -0.785) :
 			#UP
 			move(Vector2(0,-200))
-			get_node("AnimatedSprite").set_offset(Vector2(0,100))
-			get_node("AnimatedSprite/Movement_anims").play("up_walk")
-			facing = "up"
+			if is_colliding():
+				revert_motion()
+				get_node("AnimatedSprite/Idle_anims").play("idle_up")
+			else:
+				get_node("AnimatedSprite").set_offset(Vector2(0,100))
+				get_node("AnimatedSprite/Movement_anims").play("up_walk")
+				facing = "up"
 		if (angle <= -0.785 and angle >= -2.356):
 			#RIGHT
 			move(Vector2(200,0))
-			get_node("AnimatedSprite").set_offset(Vector2(-100,0))
-			get_node("AnimatedSprite/Movement_anims").play("right_walk")
+			if is_colliding():
+				revert_motion()
+				get_node("AnimatedSprite/Idle_anims").play("idle_right")
+			else:
+				get_node("AnimatedSprite").set_offset(Vector2(-100,0))
+				get_node("AnimatedSprite/Movement_anims").play("right_walk")
 			facing = "right"
 		if (angle <= 2.356 and angle >= 0.785):
 			#LEFT
 			move(Vector2(-200,0))
-			get_node("AnimatedSprite").set_offset(Vector2(100,0))
-			get_node("AnimatedSprite/Movement_anims").play("left_walk")
-			facing = "left"
+			if is_colliding():
+				revert_motion()
+				get_node("AnimatedSprite/Idle_anims").play("idle_left")
+			else:
+				get_node("AnimatedSprite").set_offset(Vector2(100,0))
+				get_node("AnimatedSprite/Movement_anims").play("left_walk")
+				facing = "left"
 		if (angle < -2.356 or angle > 2.356) :
 			#DOWN
 			move(Vector2(0,200))
-			get_node("AnimatedSprite").set_offset(Vector2(0,-100))
-			get_node("AnimatedSprite/Movement_anims").play("down_walk")
-			facing = "down"
+			if is_colliding():
+				revert_motion()
+				get_node("AnimatedSprite/Idle_anims").play("idle_down")
+			else:
+				get_node("AnimatedSprite").set_offset(Vector2(0,-100))
+				get_node("AnimatedSprite/Movement_anims").play("down_walk")
+				facing = "down"
 
 #================Movement=======================
 func _on_Movement_anims_finished():
@@ -96,7 +121,8 @@ func _on_Movement_anims_finished():
 
 #=================Idle==========================
 
-
+func _on_Idle_anims_finished():
+	next_action()
 
 #================Basic attacks==================
 
@@ -113,7 +139,7 @@ func _on_Attack_anims_windup_finished():
 	if facing == "right":
 		get_node("attack_ray1").set_cast_to(Vector2(150,50))
 		get_node("attack_ray2").set_cast_to(Vector2(150,-50))
-	
+
 	if (get_node("attack_ray1").is_colliding()):
 		var collider = get_node("attack_ray1").get_collider()
 		print(str(collider))
