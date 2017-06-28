72df1a7868482f5408b8d2506cdbd6d6214c18e8
Nathan BIETTE <nathan1.biette@free.fr>
2017-06-28 10:17:59 +0200

    1/3 chances to loot things from vases

--- Game/scripts/looter.gd
+++ Game/scripts/looter.gd
@@ -11,11 +11,11 @@ func set_location(location_to_set):
 func give_loot(current_room):
 	if (current_room == get_node("..").get_room_id()):
 		get_node("../TileMap").set_cell(location[0],location[1],13)
-		var lib = get_node("../../../..").get_ITEMS_LIBRARY()
-		var k = randi()%lib.size()
-		var item = load("res://scenes/game_hero/objects/"+str(lib[k][1])+".tscn")
-		var item_instance = item.instance()
-		get_node("..").add_child(item_instance)
-		item_instance.set_pos(get_node(".").get_pos())
-		print("free_truc")
+		if(randi()%3==0):
+			var lib = get_node("../../../..").get_ITEMS_LIBRARY()
+			var k = randi()%lib.size()
+			var item = load("res://scenes/game_hero/objects/"+str(lib[k][1])+".tscn")
+			var item_instance = item.instance()
+			get_node("..").add_child(item_instance)
+			item_instance.set_pos(get_node(".").get_pos())
 		queue_free()
