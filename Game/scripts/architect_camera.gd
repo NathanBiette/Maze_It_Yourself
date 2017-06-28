358a54408e860eaebebe5fccb3354814c63b076c
Nathan BIETTE <nathan1.biette@free.fr>
2017-06-28 15:59:13 +0200

    move camera with swipe gesture

--- Game/scripts/architect_camera.gd
+++ Game/scripts/architect_camera.gd
@@ -1,10 +1,14 @@
 extends Camera2D
 
-var previous_pos
+var previous_swipe_point
 
 func _ready():
-	previous_pos =  get_camera_pos()
+	pass
 
 
-func _on_swipe_swipe_updated( partial_gesture ):
-	get_camera_pos() + 
+func _on_swipe_swipe_updated(partial_gesture):
+	set_pos(Vector2(get_pos().x - (partial_gesture.last_point().x - previous_swipe_point.x) , get_pos().y - (partial_gesture.last_point().y - previous_swipe_point.y)))
+	previous_swipe_point = partial_gesture.last_point()
+
+func _on_swipe_swipe_started( partial_gesture ):
+	previous_swipe_point = partial_gesture.last_point()
